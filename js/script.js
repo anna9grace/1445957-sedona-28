b74946e2ec9bd7a1b99309fa1b2a160d31bbf090
anna9grace <64405475+anna9grace@users.noreply.github.com>
2020-06-15 23:42:43 +0300

    Добавляет JavaScript на форму

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var formButton = document.querySelector(".btn-open-search");
+var form = document.querySelector(".search-form");
+var checkIn = form.querySelector("[name=check-in]");
+var adults = form.querySelector("[name=adults]");
+var children = form.querySelector("[name=children]");
+var formFields = form.querySelectorAll("input:not([name=children])");
+
+var isStorageSupport = true;
+var storageAdults = "";
+var storageChildren = "";
+
+
+try {
+  storageAdults = localStorage.getItem("adults");
+  storageChildren = localStorage.getItem("children");
+} catch(err) {
+  isStorageSupport = false;
+}
+
+
+formButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  form.classList.toggle("form-hide");
+  form.classList.toggle("form-show");
+  checkIn.focus();
+
+  if (storageAdults) {
+    adults.value = storageAdults;
+  }
+
+  if (storageChildren) {
+    children.value = storageChildren;
+  }
+})
+
+
+form.addEventListener("submit", function(evt) {
+  for (formField of formFields) {
+    if (!formField.value) {
+      evt.preventDefault();
+      formField.classList.add("form-error");
+    } else {
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("children", children.value);
+    }
+  }
+})
+
+
+form.addEventListener("keydown", function(evt) {
+    for (formField of formFields) {
+    formField.classList.remove("form-error");
+    }
+  }
+)
+
